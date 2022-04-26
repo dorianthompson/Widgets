adfe20d47fb9b7197137da504d056ce4f8b0967e
Dorian Thompson <thompsondorian2@gmail.com>
2022-04-26 18:46:14 -0400

    update Search Widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
-
-    console.log('I RUN WITH EVERY RENDER');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        console.log('I RUN AT EVERY RENDER AND AT INITIAL RENDER');
+        const search = async () => {
+            const{ data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+
+        if(term){
+            search();
+        }
+
     }, [term]);
+
+    const renderedResult = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -20,6 +50,9 @@ const Search = () => {
                         className='input'
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResult}
             </div>
         </div>
 
